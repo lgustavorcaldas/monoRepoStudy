38cea61c97c86607164e0476cc107532c4ce501e
lgustavorcaldas <lgustavorcaldas>
2022-05-12 15:46:54 -0300

    more commit

--- frontend/index.js
+++ frontend/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use("/", express.static("./src/home"))
+
 app.listen(port, console.log(`FrontEnd running at port: ${port}`))
